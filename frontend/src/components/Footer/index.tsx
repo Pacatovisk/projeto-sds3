575f1b0294e9ae5c2a67ec8f4d756de5d7adad41
Marcelo Viana <marcelovianadias@outlook.com>
2021-05-07 17:02:46 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+          <p className="text-light">App desenvolvido por <a href="https://github.com/pacatovisk" target="_blank" rel="noreferrer">Marcelo Viana Dias</a></p>
+          <p className="text-light"><small><strong>Semana Spring React</strong></small></p>
+        </div>
+      </footer>
+      
+    );
+  }
+  
+  export default Footer;
