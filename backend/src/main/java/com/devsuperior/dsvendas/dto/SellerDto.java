d75c0cf803296190a08724a4ee6ed6f6958dff7d
marcelo.dias <marcelovianadias@outlook.com>
2021-05-08 10:59:42 -0300

    Layers

---
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SellerDto.java
@@ -0,0 +1,37 @@
+package com.devsuperior.dsvendas.dto;
+
+import com.devsuperior.dsvendas.entities.Seller;
+
+import java.io.Serializable;
+
+public class SellerDto implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+
+    public SellerDto() {
+
+    }
+
+    public SellerDto(Seller entity) {
+        id = entity.getId();
+        name = entity.getName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
