6b89b7ab1fb996a2fef3059a05bf3b475abc542a
Marcelo Viana <marcelovianadias@outlook.com>
2021-05-09 13:50:59 -0300

    Env var

--- frontend/src/utils/resquest.ts
+++ frontend/src/utils/resquest.ts
@@ -1,4 +1,4 @@
 //export const BASE_URL =  'https://sds3-pacatovisk.herokuapp.com';
 
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'https://sds3-pacatovisk.herokuapp.com';
 
